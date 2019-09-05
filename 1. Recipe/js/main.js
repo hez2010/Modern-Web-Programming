9481b0777e3523039a4a0659ef51fcab340103ee
Steven He <hez2010@outlook.com>
2019-09-05 16:12:26 +0800

    Finish 1. Recipe

--- 1. Recipe/js/main.js
+++ 1. Recipe/js/main.js
@@ -1,3 +1,24 @@
+// remove preloader mask if page loaded
 document.body.onload = function () {
-    document.querySelector('loader').remove();
+    const preloader = document.querySelector('preloader');
+    preloader.setAttribute('class', 'fadeout-preloader');
+    setTimeout(function () { preloader.remove(); }, 500);
+};
+
+// event listener for togglers
+document.querySelectorAll('.toggler')
+    .forEach(function (v) {
+        v.addEventListener('click', function () { toggleBlock(v) });
+    });
+
+function toggleBlock(target) {
+    const element = document.querySelector('#' + target.getAttribute('data-target'));
+    if (element.style.display === 'block') {
+        element.style.display = 'none';
+        target.setAttribute('class', 'toggler toggler-collapsed');
+    }
+    else {
+        element.style.display = 'block';
+        target.setAttribute('class', 'toggler toggler-expanded');
+    }
 }
