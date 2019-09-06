bab3e5f140fd13f7d571354d021eb24db8c12565
Steven He <hez2010@outlook.com>
2019-09-06 15:02:25 +0800

    remove toggler

--- 1. Recipe/js/main.js
+++ 1. Recipe/js/main.js
@@ -5,20 +5,3 @@ document.body.onload = function () {
     setTimeout(function () { preloader.remove(); }, 500);
 };
 
-// event listener for togglers
-document.querySelectorAll('.toggler')
-    .forEach(function (v) {
-        v.addEventListener('click', function () { toggleBlock(v) });
-    });
-
-function toggleBlock(target) {
-    const element = document.querySelector('#' + target.getAttribute('data-target'));
-    if (element.style.display === 'block') {
-        element.style.display = 'none';
-        target.setAttribute('class', 'toggler toggler-collapsed');
-    }
-    else {
-        element.style.display = 'block';
-        target.setAttribute('class', 'toggler toggler-expanded');
-    }
-}
