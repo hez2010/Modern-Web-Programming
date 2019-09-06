275702ffa238e8269ee643c1a99ee942af2be3ee
Steven He <hez2010@outlook.com>
2019-09-06 18:42:07 +0800

    fixes

--- 1. Recipe/js/main.js
+++ 1. Recipe/js/main.js
@@ -1,6 +1,6 @@
 // remove preloader mask if page loaded
 document.body.onload = function () {
-    const preloader = document.querySelector('preloader');
+    const preloader = document.querySelector('#preloader');
     preloader.setAttribute('class', 'fadeout-preloader');
     setTimeout(function () { preloader.remove(); }, 500);
 };
