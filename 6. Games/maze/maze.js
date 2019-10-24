f27da9d4ee79bf57280c8a53acfa7fca98dd9bac
Steven He <hez2010@outlook.com>
2019-10-24 23:09:31 +0800

    Improve maze code

--- 6. Games/maze/maze.js
+++ 6. Games/maze/maze.js
@@ -15,21 +15,9 @@ window.onload = () => {
   let started = false;
   let isCheat = false;
 
-  let setWall = wall => {
-    wall.setAttribute("moved", "");
-  }
-
-  let resetWall = wall => {
-    wall.removeAttribute("moved");
-  }
-
   let setStatus = message => {
     status.textContent = message;
     status.setAttribute("show", "");
-  }
-
-  let resetStatus = () => {
-    status.removeAttribute("show");
   }
 
   maze.addEventListener("mouseleave", () => {
@@ -39,25 +27,22 @@ window.onload = () => {
 
   walls.forEach(v => {
     v.addEventListener("mouseover", () => {
-      if (started) {
-        setWall(v);
-        setStatus("You Lose!");
-        started = false;
-      }
+      if (!started) returnl
+      v.setAttribute("touched", "");
+      setStatus("You Lose!");
+      started = false;
     });
   })
 
   start.addEventListener("mouseover", () => {
-    walls.forEach(v => resetWall(v));
-
-    resetStatus();
+    walls.forEach(v => v.removeAttribute("touched"));
+    status.removeAttribute("show");
     started = true;
     isCheat = false;
   });
 
   end.addEventListener("mouseover", () => {
     if (!started) return;
-
     setStatus(isCheat ? "Don't cheat, you should start from the 'S' and move to the 'E' inside the maze!" : "You Win!");
     started = false;
   });
